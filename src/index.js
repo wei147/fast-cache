26d3749b9daef3dac28ba72b12d99b8da580a9f8
wei <1216381706@qq.com>
2019-12-06 22:20:28 +0800

    V0.0.1 初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert ('fast-cache');
+class FastCache {
+	constructor(){
+		this.list = {}
+	}
+	set(key,value) {
+		this.list[key] = value
+    }
+    get(key) {
+    	return this.list[key]
+    }
+	
+}
+
+window.FastCache = FastCache
